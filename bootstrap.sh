21c1ad8948e7730cf5cfb3e4d8efab88978642c4
mdoug <mdoug@pop-os.localdomain>
2022-11-21 22:29:08 -0300

    cross-origin

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export FLASK_APP=./chs/index.py
-pipenv run flask --debug run -h 0.0.0.0
+python3 -m pipenv run flask --debug run -h 0.0.0.0
--- chs/index.py
+++ chs/index.py
@@ -1,20 +1,25 @@
 from flask import Flask, jsonify
+from flask_cors import cross_origin
 from chs.indicadores.compras_anteriores import ComprasAnteriores
 
 app = Flask(__name__)
 
+
+@cross_origin()
 @app.route("/")
 def hello_world():
     return "E aí Mundo!"
 
 #endpoint para o indicador por mês e ano
 @app.route("/indicador_compras_anteriores/<int:mes>/<int:ano>")
+@cross_origin()
 def get_compras_anteriores(mes, ano):
     indicadores = ComprasAnteriores().get_indicador_meses_anteriores(mes, ano)
     return jsonify(indicadores)
 
 #endpoint para o indicador por mês e ano
 @app.route("/resumo_indicador_compras_anteriores/<int:mes>/<int:ano>")
+@cross_origin()
 def get_resumo_compras_anteriores(mes, ano):
     indicadores = ComprasAnteriores().get_indicador_resumo(mes, ano)
     return jsonify(indicadores)
